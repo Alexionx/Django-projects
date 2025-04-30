e42b48ba7af7cd1244d18ee65f8b8d0ef5755a02
KempachiDZ <alexmelnyk48370@gmail.com>
2025-04-30 13:37:41 +0300

    Очистка репозиторію перед новими практичними

--- mysite/app_blog/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-from .models import ArticleImage
-
-class ArticleImageForm(forms.ModelForm):
-    image = forms.ImageField(widget=forms.ClearableFileInput())
-
-    class Meta:
-        model = ArticleImage
-        fields = '__all__'
